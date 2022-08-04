9ea49db83e59e80b3162152ba4629bce1ed5a6b7
redekarnr <64104024+redekarnr@users.noreply.github.com>
2022-08-04 17:15:44 -0400

    Add files via upload

---
+++ scripts/create_contrasts_file.R
@@ -0,0 +1,26 @@
+
+#### wrapper to prepare sample.txt for DMR analyses
+args <- commandArgs(trailingOnly = TRUE)
+
+wd <- as.character(args[1])
+group1=as.character(args[2])
+group2=as.character(args[3])
+
+setwd(wd)
+
+meta = read.table(paste(wd, "/samples.txt", sep=""), header=T)
+colnames(meta) = c("samples", "group")
+contrast = as.character(paste(group1, "-vs-", group2, sep=""))
+
+data = meta[which(meta$group %in% c(group1, group2)),]
+
+CpGpath = NULL
+for(i in 1:nrow(data)){
+  path = paste(wd, "/CpG/", data$samples[i], ".bismark_bt2_pe.deduplicated.CpG_report.txt.gz", sep="")
+  data$comparisons[i] = contrast
+  data$path[i] = path
+}
+
+write.table(data, paste(wd, "/contrasts.txt", sep=""), sep = "\t", append = FALSE, row.names = FALSE, quote = FALSE)
+
+
